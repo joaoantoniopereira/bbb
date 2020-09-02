76db2c6c87f17d41d9bfc1eb70226b8c14676a57
joaoantoniopereira <joao.antonio.pereira@outlook.com>
2020-09-02 19:43:00 +0100

    Updates

---
+++ precache-manifest.e2022d5376b4a83338653763241ae2b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b893fb97474240e0113d2d8af2803034",
+    "url": "/bbb/index.html"
+  },
+  {
+    "revision": "746b06c7f0f32a90c7bb",
+    "url": "/bbb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b22658763a4cef04c47",
+    "url": "/bbb/static/js/2.35f356f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bbb/static/js/2.35f356f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746b06c7f0f32a90c7bb",
+    "url": "/bbb/static/js/main.e157f299.chunk.js"
+  },
+  {
+    "revision": "416fdbd470f07e191253",
+    "url": "/bbb/static/js/runtime-main.367fb8d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bbb/static/media/logo.5d5d9eef.svg"
+  }
+]);
